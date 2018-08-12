7dafc2f43381e18ae7f23457c8526605431296a9
Lukas Haidacher <lukas.haidacher@pdagroup.net>
2018-08-12 20:13:11 +0200

    vue video browser

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="container">
+        <SearchBar @termChange="onTermChange"></SearchBar> <!-- alt: v-on:termChange -->
+        <div class="row">
+            <VideoDetail :video="selectedVideo"></VideoDetail>
+            <VideoList :videos="videos" @videoSelect="onVideoSelect"></VideoList> <!-- alt: v-bind:videos -->
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import axios from 'axios';
+    import SearchBar from './components/SearchBar';
+    import VideoList from './components/VideoList';
+    import VideoDetail from './components/VideoDetail';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    const API_KEY = 'AIzaSyBu6kmQZvvLMQRAdDE8O24-F-WNmtuvy_o';
+
+    export default {
+        name: 'App',
+
+        components: {
+            SearchBar,
+            VideoList,
+            VideoDetail
+        },
+
+        data: function () {
+            return {
+                videos: [],
+                selectedVideo: null
+            };
+        },
+
+        methods: {
+            onTermChange: function (searchTerm) {
+                axios.get('https://www.googleapis.com/youtube/v3/search', {
+                    params: {
+                        key: API_KEY,
+                        type: 'video',
+                        part: 'snippet',
+                        q: searchTerm
+                    }
+                }).then(response => {
+                    this.videos = response.data.items;
+                });
+            },
+            onVideoSelect: function (video) {
+                this.selectedVideo = video;
+            }
+        }
+    };
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/VideoListItem.vue
@@ -0,0 +1,43 @@
+<template>
+    <li class="list-group-item media" @click="onVideoSelect">
+        <img class="mr-3"
+             alt="Image"
+             :src="thumbnailUrl">
+        <div class="media-body">
+            {{ video.snippet.title }}
+        </div>
+    </li>
+</template>
+
+<script>
+    export default {
+        name: 'VideoListItem',
+
+        props: {
+            video: Object
+        },
+
+        computed: {
+            thumbnailUrl: function () {
+                return this.video.snippet.thumbnails.default.url;
+            }
+        },
+
+        methods: {
+            onVideoSelect: function() {
+                this.$emit('videoSelect', this.video);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    li {
+        display: flex;
+        cursor: pointer;
+    }
+
+    li:hover {
+        background-color: #eee;
+    }
+</style>
